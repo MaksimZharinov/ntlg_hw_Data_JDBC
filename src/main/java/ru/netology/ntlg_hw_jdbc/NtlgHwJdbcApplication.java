dabd7d2604606ac558ace3ca04191b086d96dccb
Maxim Zharinoff <minimum.cold1991@gmail.com>
2025-02-25 08:17:07 +0700

    init commit

---
+++ src/main/java/ru/netology/ntlg_hw_jdbc/NtlgHwJdbcApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.ntlg_hw_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtlgHwJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtlgHwJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/netology/ntlg_hw_jdbc/NtlgHwJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.ntlg_hw_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtlgHwJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
